be12df37dfc13611866f4709693c508ab63b5494
Liljenmaa <jesse.liljenmaa@gmail.com>
2023-08-21 11:25:53 +0300

    Made locked fields in Yahtzee stand out more

--- src/components/subcomponents/YahtzeeRow.js
+++ src/components/subcomponents/YahtzeeRow.js
@@ -3,8 +3,8 @@ import React from 'react';
 const YahtzeeRow = ({ title, lockedScore, score, scoreFunc, notAllowed }) =>
   <div className="scoreboard-row">
     <div className="scoreboard-field scoreboard-title">{ title }</div>
-    { lockedScore !== null ? 
-      <div className="scoreboard-field scoreboard-score"><b>{ lockedScore }</b></div> :
+    { lockedScore !== null ?
+      <div className="scoreboard-field scoreboard-score"><b>~{ lockedScore }~</b></div> :
       <div className={"scoreboard-field scoreboard-score " + (notAllowed ? "" : "clickable")} onClick={() => scoreFunc(score)}>{ score }</div>
     }
     {
